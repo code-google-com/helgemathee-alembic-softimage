0af9ac9e92660f175b90ced63b00ed20d465de7a
helge <helge@helge-fabric.EUVR>
2011-08-24 12:11:34 +0200

    Fixing the build process for debug and static boost libs.

--- init_Alembic.cmd
+++ init_Alembic.cmd
@@ -11,12 +11,18 @@ REM ****************************************************************************
 REM let's assume we don't need to debug IlmBase, boost, or zlib and just use the non-debug
 REM libs even when building debug. This way we don't have to nuke the projects to switch.
 REM ******************************************************************************************
-:	set DB=_db
+	set DB=_db
 	set config=Debug
+	set boost_suffix=-sgd
+	set cmake_flags=/MTd
+	set zlib_suffix=d
 	shift
 ) ELSE (
 	set config=RelWithDebInfo
 	set DB=
+	set boost_suffix=-s
+	set zlib_suffix=
+	set cmake_flags=/MT /U _DEBUG
 )
 
 python "%ALEMBIC_ROOT%\convert_ilmbase_vcprojs.py"
@@ -42,11 +48,6 @@ set ZLIB_ROOT=%ALEMBIC_ROOT%\thirdparty\zlib-1.2.5
 
 
 REM ******************************************************************************************
-REM We always use the MT libraries, so comment out the next line if you want MTd libs for debug
-REM ******************************************************************************************
-set cmake_flags=/MT /U _DEBUG
-
-REM ******************************************************************************************
 REM Corrections to vars in bootstrap script
 REM ******************************************************************************************
 set warnFlags=/wd4267 /wd4800 /wd4018 /wd4244 %cmake_flags%
@@ -55,10 +56,10 @@ set cppflags=%ccflags% /EHsc /GR
 
 set BASE_ARGS=--disable-prman --disable-maya --generator=%Generator%
 rem set BASE_ARGS=--disable-prman --with-maya=%MAYA_ROOT% --generator=%Generator%
-set HDF_ARGS=--hdf5_include_dir=%platOut%\hdf5\include --hdf5_hdf5_library=%platOut%\hdf5\bin\RelWithDebInfo\hdf5_hl%db%.lib
+set HDF_ARGS=--hdf5_include_dir=%platOut%\hdf5\include --hdf5_hdf5_library=%platOut%\hdf5\bin\%config%\hdf5_hl%zlib_suffix%.lib
 set ILM_ARGS=--ilmbase_include_dir=%ILMBASE_ROOT%\include --ilmbase_imath_library=%ILMBASE_ROOT%\lib%db%\%config%\joined.lib
-set BOOST_ARGS=--boost_include_dir=%platOut%\boost\include\boost-1_42 --boost_program_options_library=%platOut%\boost\lib%db%\libboost_program_options-vc90-mt%db%-1_42.lib
-set ZLIB_ARGS=--zlib_include_dir=%ZLIB_ROOT% --zlib_library=%platOut%\zlib\RelWithDebInfo\zlib%db%.lib
+set BOOST_ARGS=--boost_include_dir=%platOut%\boost\include\boost-1_42 --boost_program_options_library=%platOut%\boost\lib%DB%\libboost_program_options-vc90-mt%boost_suffix%-1_42.lib
+set ZLIB_ARGS=--zlib_include_dir=%ZLIB_ROOT% --zlib_library=%platOut%\zlib\%config%\zlib%zlib_suffix%.lib
 
 @echo on
 pushd %platOut%
