9340e4037ac488c36f2c52c2dd90aca409947641
helge <helge@helge-fabric.EUVR>
2011-08-24 14:47:37 +0200

    First empty softimage plugin using the alembic libraries, crashing right now.

---
+++ softimage/alembic/Application/Plugins/Foundation.h
@@ -0,0 +1,41 @@
+#ifndef _FOUNDATION_H_
+#define _FOUNDATION_H_
+
+#include <boost/smart_ptr.hpp>
+#include <boost/format.hpp>
+#include <boost/variant.hpp>
+
+#include <utility>
+#include <limits>
+
+#include <set>
+#include <vector>
+#include <map>
+#include <list>
+
+#include <stdexcept>
+#include <exception>
+
+#include <string>
+
+#include <math.h>
+#include <float.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include <Alembic/AbcGeom/All.h>
+#include <Alembic/Abc/OArrayProperty.h>
+#include <Alembic/Abc/OScalarProperty.h>
+#include <Alembic/AbcGeom/OXform.h>
+#include <Alembic/AbcGeom/XformOp.h>
+#include <Alembic/AbcGeom/OPoints.h>
+#include <Alembic/AbcGeom/OPolyMesh.h>
+#include <Alembic/AbcGeom/OSubD.h>
+#include <Alembic/AbcGeom/ONuPatch.h>
+#include <Alembic/AbcGeom/OCurves.h>
+#include <Alembic/AbcGeom/OCamera.h>
+#include <Alembic/AbcCoreHDF5/All.h>
+
+#endif  // _FOUNDATION_H_
